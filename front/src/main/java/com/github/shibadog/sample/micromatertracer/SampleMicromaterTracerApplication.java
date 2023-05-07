641cea5bd339288456737d8d7db3808738346ab2
shibadog <shiba_dog@outlook.com>
2023-05-07 00:29:56 +0000

    init

---
+++ front/src/main/java/com/github/shibadog/sample/micromatertracer/SampleMicromaterTracerApplication.java
@@ -0,0 +1,13 @@
+package com.github.shibadog.sample.micromatertracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMicromaterTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMicromaterTracerApplication.class, args);
+	}
+
+}
---
+++ front/src/test/java/com/github/shibadog/sample/micromatertracer/SampleMicromaterTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shibadog.sample.micromatertracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMicromaterTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
